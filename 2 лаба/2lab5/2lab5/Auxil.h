ccbfce8048d694dfd3bc19e6971101ea62ccc554
Mash Ok <90055466+mashokk@users.noreply.github.com>
2022-03-24 10:50:40 +0300

    Add files via upload

---
+++ 2 лаба/2lab5/2lab5/Auxil.h
@@ -0,0 +1,11 @@
+//Auxil.h
+#pragma once
+#include <cstdlib>
+#include <ctime>
+
+namespace auxil
+{
+	void   start();						
+	double dget(double rmin, double rmax);
+	int    iget(int rmin, int rmax);	
+};
